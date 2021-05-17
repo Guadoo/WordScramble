12241213fd67d6070841885dddcd63a3fb9460cb
Liu Jingkai <rock_jk@msn.com>
2021-05-17 15:05:32 +0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Guadoo on 2021/5/17.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
